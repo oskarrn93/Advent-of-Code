eed16e75f2ed9e2b7d1275ad50bf0acf7b4af1f1
Oskar Rosén <oskar@oskarrosen.com>
2019-12-02 22:03:58 +0100

    answer is 7210630

---
+++ 2019/day2/part1.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+
+const inputFile = "./input.txt";
+
+let data = null;
+let index = 0;
+
+const HALT_CODE = 99;
+const ADD_CODE = 1;
+const MULTIPLY_CODE = 2;
+
+try {
+  data = fs
+    .readFileSync(inputFile, "utf8")
+    .trim()
+    .split(",")
+    .map(item => parseInt(item));
+} catch (error) {
+  console.error(error);
+  exit();
+}
+
+while (true) {
+  const opCode = data[index];
+
+  if (opCode === HALT_CODE) {
+    break;
+  } else if (opCode === ADD_CODE) {
+    const inputPositionOne = data[index + 1];
+    const inputPositionTwo = data[index + 2];
+    const outputPosition = data[index + 3];
+
+    data[outputPosition] = data[inputPositionOne] + data[inputPositionTwo];
+  } else if (opCode === MULTIPLY_CODE) {
+    const inputPositionOne = data[index + 1];
+    const inputPositionTwo = data[index + 2];
+    const outputPosition = data[index + 3];
+
+    data[outputPosition] = data[inputPositionOne] * data[inputPositionTwo];
+  } else {
+    console.error(`Invalid opCode: ${opCode}`);
+  }
+
+  index += 4;
+}
+
+console.log(`Answer is: ${data[0]}`);
