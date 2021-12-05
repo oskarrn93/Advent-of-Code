a01506b7154d6ddf1ed8fba61b57fda811155cc8
Oskar Rosén <oskar@oskarrosen.com>
2021-12-05 03:10:54 +0100

    2021 day 3 part 2

--- 2021/day3/main.go
+++ 2021/day3/main.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+var BINARY_STRING_INPUT_LENGTH int = 12
+
 func readInputFromFile() []string {
 	data, error := os.ReadFile("./input.txt")
 	if error != nil {
@@ -57,7 +59,7 @@ func calculateFrequencyPerPosition(input []string, frequency *map[int]int) {
 func isMostCommonBitOne(index int, frequency *map[int]int) bool {
 	value := (*frequency)[index]
 
-	if value > 0 {
+	if value >= 0 {
 		return true
 	}
 	return false
@@ -89,6 +91,62 @@ func generateEpsilonRate(frequency *map[int]int) string {
 	return strings.Join(result, "")
 }
 
+func filterInputByBitIndexValue(input []string, index int, value int) []string {
+	result := []string{}
+
+	for _, inputValue := range input {
+		charValue := convertStringToInt(string([]rune(inputValue)[index]))
+
+		if charValue == value {
+			result = append(result, inputValue)
+		}
+	}
+
+	return result
+}
+
+func generateOxygenGeneratorRate(input []string) string {
+	filteredInput := input
+
+	for index := 0; index < BINARY_STRING_INPUT_LENGTH; index++ {
+		frequency := make(map[int]int)
+		calculateFrequencyPerPosition(filteredInput, &frequency)
+
+		if isMostCommonBitOne(index, &frequency) {
+			filteredInput = filterInputByBitIndexValue(filteredInput, index, 1)
+		} else {
+			filteredInput = filterInputByBitIndexValue(filteredInput, index, 0)
+		}
+
+		if len(filteredInput) == 1 {
+			break
+		}
+	}
+
+	return filteredInput[0]
+}
+
+func generateCO2ScrubberRate(input []string) string {
+	filteredInput := input
+
+	for index := 0; index < BINARY_STRING_INPUT_LENGTH; index++ {
+		frequency := make(map[int]int)
+		calculateFrequencyPerPosition(filteredInput, &frequency)
+
+		if isMostCommonBitOne(index, &frequency) {
+			filteredInput = filterInputByBitIndexValue(filteredInput, index, 0)
+		} else {
+			filteredInput = filterInputByBitIndexValue(filteredInput, index, 1)
+		}
+
+		if len(filteredInput) == 1 {
+			break
+		}
+	}
+
+	return filteredInput[0]
+}
+
 func part1() {
 	fmt.Println("Advent of Code day 3 part 1")
 
@@ -105,6 +163,19 @@ func part1() {
 	fmt.Println("Answer:", powerConsumption)
 }
 
+func part2() {
+	fmt.Println("Advent of Code day 3 part 2")
+
+	input := readInputFromFile()
+
+	oxygenRate := generateOxygenGeneratorRate(input)
+	co2Rate := generateCO2ScrubberRate(input)
+
+	lifeSupportRating := convertBinaryStringToInt(oxygenRate) * convertBinaryStringToInt(co2Rate)
+	fmt.Println("Answer:", lifeSupportRating)
+}
+
 func main() {
 	part1()
+	part2()
 }
