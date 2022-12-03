c7f1cf2f00a174d2ec142f15ae27f80fc63e6e05
Oskar Rosén <oskar@oskarrosen.com>
2022-12-03 15:57:18 +0100

    refactor

--- 2022/day3/main.exs
+++ 2022/day3/main.exs
@@ -19,16 +19,24 @@ defmodule Day3 do
     |> String.split("\n")
   end
 
+  def get_overlapping_character([a, b]) do
+    Enum.join(MapSet.to_list(MapSet.intersection(MapSet.new(a), MapSet.new(b))))
+  end
+
+  def get_overlapping_character([a, b, c]) do
+    Enum.join(
+      MapSet.to_list(
+        MapSet.intersection(MapSet.intersection(MapSet.new(a), MapSet.new(b)), MapSet.new(c))
+      )
+    )
+  end
+
   def part1 do
     setup()
     |> Enum.map(&String.split_at(&1, Integer.floor_div(String.length(&1), 2)))
     |> Enum.map(&Tuple.to_list(&1))
     |> Enum.map(&Enum.map(&1, fn x -> String.graphemes(x) end))
-    |> Enum.map(fn [a, b] ->
-      MapSet.intersection(MapSet.new(a), MapSet.new(b))
-    end)
-    |> Enum.map(&MapSet.to_list(&1))
-    |> Enum.map(&Enum.join(&1))
+    |> Enum.map(&get_overlapping_character(&1))
     |> Enum.map(&convert_character_to_value(&1))
     |> Enum.sum()
     |> IO.inspect(label: "Part 1")
@@ -38,11 +46,7 @@ defmodule Day3 do
     setup()
     |> Enum.chunk_every(3)
     |> Enum.map(&Enum.map(&1, fn x -> String.graphemes(x) end))
-    |> Enum.map(fn [a, b, c] ->
-      MapSet.intersection(MapSet.intersection(MapSet.new(a), MapSet.new(b)), MapSet.new(c))
-    end)
-    |> Enum.map(&MapSet.to_list(&1))
-    |> Enum.map(&Enum.join(&1))
+    |> Enum.map(&get_overlapping_character(&1))
     |> Enum.map(&convert_character_to_value(&1))
     |> Enum.sum()
     |> IO.inspect(label: "Part 2")
