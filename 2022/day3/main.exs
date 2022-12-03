09664563c1c7e901177a2e5fc5f4acdeb005a24b
Oskar Rosén <oskar@oskarrosen.com>
2022-12-03 15:11:05 +0100

    refactor

--- 2022/day3/main.exs
+++ 2022/day3/main.exs
@@ -1,4 +1,16 @@
 defmodule Day3 do
+  def convert_character_to_value(char) do
+    ascii_char = :binary.first(char)
+
+    # convert from ascii value
+    cond do
+      # lower case char
+      ascii_char >= 97 -> ascii_char - 96
+      # upper case char
+      true -> ascii_char - 38
+    end
+  end
+
   def setup() do
     {:ok, input} = File.read("input.txt")
 
@@ -8,26 +20,17 @@ defmodule Day3 do
     |> Enum.map(&String.split_at(&1, Integer.floor_div(String.length(&1), 2)))
     |> Enum.map(&Tuple.to_list(&1))
     |> Enum.map(&Enum.map(&1, fn x -> String.graphemes(x) end))
+  end
+
+  def part1 do
+    setup()
     |> Enum.map(fn [a, b] ->
       MapSet.intersection(MapSet.new(a), MapSet.new(b))
     end)
     |> Enum.map(&MapSet.to_list(&1))
     |> Enum.map(&Enum.join(&1))
-    |> Enum.map(&:binary.first(&1))
-    |> Enum.map(fn x ->
-      # convert from ascii value
-      cond do
-        # lower case char
-        x >= 97 -> x - 96
-        # upper case char
-        true -> x - 38
-      end
-    end)
+    |> Enum.map(&convert_character_to_value(&1))
     |> Enum.sum()
-  end
-
-  def part1 do
-    setup()
     |> IO.inspect(label: "Part 1")
   end
 end
